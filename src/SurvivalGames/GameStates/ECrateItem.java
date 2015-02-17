3ba44bcbcdc1a187c387ca572f85eb5066f6f10c
Thecheatgamer1 <connor_shearer@hotmail.co.uk>
2015-02-17 13:33:47 +0000

    Little alteration

--- src/SurvivalGames/GameStates/ECrateItem.java
+++ src/SurvivalGames/GameStates/ECrateItem.java
@@ -4,21 +4,15 @@ import org.bukkit.Material;
 
 public enum ECrateItem {
 	
-	/*
-	 * 
-	 * 
-	 * 
-	 */
-	
 	// Tier 1
 	
-	ARROW(Material.ARROW, ECrateTiers.TIER1, 75),
-	WOODEN_SWORD(Material.WOOD_SWORD, ECrateTiers.TIER1, 60),
-	MELON(Material.MELON, ECrateTiers.TIER1, 95),
-	LEATHER_HELMENT(Material.LEATHER_HELMET, ECrateTiers.TIER1, 40),
-	LEATHER_CHESTPLATE(Material.LEATHER_CHESTPLATE ,ECrateTiers.TIER1, 20),
-	LEATHER_LEGGINGS(Material.LEATHER_LEGGINGS, ECrateTiers.TIER1, 30),
-	LEATHER_BOOTS(Material.LEATHER_BOOTS, ECrateTiers.TIER1, 50);
+	ARROW1(Material.ARROW, ECrateTiers.TIER1, 75),
+	WOODEN_SWORD1(Material.WOOD_SWORD, ECrateTiers.TIER1, 60),
+	MELON1(Material.MELON, ECrateTiers.TIER1, 95),
+	LEATHER_HELMENT1(Material.LEATHER_HELMET, ECrateTiers.TIER1, 40),
+	LEATHER_CHESTPLATE1(Material.LEATHER_CHESTPLATE ,ECrateTiers.TIER1, 20),
+	LEATHER_LEGGINGS1(Material.LEATHER_LEGGINGS, ECrateTiers.TIER1, 30),
+	LEATHER_BOOTS1(Material.LEATHER_BOOTS, ECrateTiers.TIER1, 50);
 	
 	// Tier 2
 	
@@ -28,24 +22,17 @@ public enum ECrateItem {
 	
 	// Tier 5
 	
-	
-	
-	private final Material material;
-	private final ECrateTiers tier;
-	private final int chanceToSpawnInTier;
+	private Material material;
+	private ECrateTiers tier;
+	private int chanceToSpawnInTier;
 	
 	ECrateItem(Material material, ECrateTiers tier, int chanceToSpawnInTier) {
 		this.material = material;
 		this.tier = tier;
 		this.chanceToSpawnInTier = chanceToSpawnInTier;
-		
-		
-		/*
-		 * 
-		 */
 	}
 	
-	public Material getItemMaterial() {return material;}
-	public ECrateTiers getItemTier() {return tier;};
-	public int getChanceToSpawn() {return chanceToSpawnInTier;}
+	public Material getItemMaterial(ECrateItem item) {return material;}
+	public ECrateTiers getItemTier(ECrateItem item) {return tier;}
+	public int getChanceToSpawn(ECrateItem item) {return chanceToSpawnInTier;}
 }
